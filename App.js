b48f01f48cf83be1d058abb00aa5ae182e3cae9a
Tariq Hijazi <Tariq.t.m.hijazi@gmail.com>
2022-10-28 22:13:17 +0300

    first

---
+++ App.js
@@ -0,0 +1,28 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import messaging from '@react-native-firebase/messaging';
+const App = () => {
+  React.useEffect(() => {
+    (async function requestUserPermission() {
+      const authStatus = await messaging().requestPermission(); //send req to user to allow notification
+      const enabled = //check requestPermission
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+      if (enabled) {
+        messaging()
+          .getToken()
+          .then(val => console.log(val));
+      }
+    })();
+  }, []);
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
